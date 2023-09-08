f3cfda1cddd772c8e46cff17308535123d0b8f8e
chi minh toi a <dinhbang121@gmail.com>
2023-09-08 15:13:26 +0700

    search algorithm

---
+++ src/Controller/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Controller;
+
+import Common.Library;
+import Model.Element;
+
+/**
+ *
+ * @author admin
+ */
+public class Main {
+public static void main(String[] args) {
+        Library library = new Library();
+        Element element = new Element();
+        //1. Enter a possitive number
+        element.setSize_array(library.getInt("Enter number of array", 1, 100));
+        //2. Create array by generate random integer in range
+        element.setArray(library.createArray(element.getSize_array()));
+        new SortProgramming(element).run();
+    }
+    
+}
